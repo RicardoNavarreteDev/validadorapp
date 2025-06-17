d1601ac04bac5698bb3ba65713535b1ac04b52f8
RicardoN <navarrete.soto90@gmail.com>
2025-06-17 17:55:33 -0400

    Subida inicial del proyecto validadorapp

---
+++ src/main/java/com/equipo/validador/App.java
@@ -0,0 +1,13 @@
+package com.equipo.validador;
+
+/**
+ * Hello world!
+ */
+public class App {
+    public static void main(String[] args) {
+        String usuario = System.getenv("APP_USER");
+        if ("admin".equals(usuario)){
+             System.out.println("Bienvendio Administrador!");
+        }
+    }
+}
